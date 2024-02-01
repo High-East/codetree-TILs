c030101feb669e8421e28a2bc8053e5ef8020979
DongHee <rhehd127@gmail.com>
2024-02-01 15:37:19 +0900

    Title: 트로미노, Time: 128ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240201/트로미노/tromino.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+max_sum = 0
+
+# check first block
+for i in range(n - 1):
+    for j in range(m - 1):
+        block = [arr[i][j], arr[i + 1][j], arr[i][j + 1], arr[i + 1][j + 1]]
+        max_sum = max(max_sum, sum(block) - min(block))
+
+# check horizontal block
+for i in range(n):
+    for j in range(m - 2):
+        block = arr[i][j: j + 2]
+        max_sum = max(max_sum, sum(block) - min(block))
+
+# check vertical block
+for i in range(n - 2):
+    for j in range(m):
+        block = [arr[i][j], arr[i + 1][j], arr[i + 2][j]]
+        max_sum = max(max_sum, sum(block) - min(block))
+
+print(max_sum)
