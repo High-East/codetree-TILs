1d76e5a7429fc45eda3c49cc092ac8fbf197a23f
DongHee <rhehd127@gmail.com>
2024-02-01 20:26:45 +0900

    Title: 두 정수에 대한 연산값 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240201/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
+++ 240201/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.py
@@ -3,4 +3,6 @@ a, b = map(int, input().split())
 def cal(a, b):
     return min([a, b]) + 10, max([a, b]) * 2
 
-print(*cal(a, b), sep=' ')
+result = cal(a, b)
+result.sort()
+print(*result, sep=' ')
