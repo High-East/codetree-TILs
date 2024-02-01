3495c3c320fa657fda1cd68477ea55bc2deb5b14
DongHee <rhehd127@gmail.com>
2024-02-01 15:37:54 +0900

    Title: 트로미노, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240201/트로미노/tromino.py
+++ 240201/트로미노/tromino.py
@@ -12,12 +12,12 @@ for i in range(n - 1):
 for i in range(n):
     for j in range(m - 2):
         block = arr[i][j: j + 2]
-        max_sum = max(max_sum, sum(block) - min(block))
+        max_sum = max(max_sum, sum(block))
 
 # check vertical block
 for i in range(n - 2):
     for j in range(m):
         block = [arr[i][j], arr[i + 1][j], arr[i + 2][j]]
-        max_sum = max(max_sum, sum(block) - min(block))
+        max_sum = max(max_sum, sum(block))
 
 print(max_sum)
