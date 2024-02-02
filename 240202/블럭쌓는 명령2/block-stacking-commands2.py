c38f21557ed00f652e54028866e47ce2d03a3bcb
DongHee <rhehd127@gmail.com>
2024-02-02 22:19:05 +0900

    Title: 블럭쌓는 명령2, Time: 140ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240202/블럭쌓는 명령2/block-stacking-commands2.py
@@ -0,0 +1,7 @@
+N, K = map(int, input().split())
+arr = [0 for _ in range(N + 1)]
+for _ in range(K):
+    a, b = map(int, input().split())
+    for i in range(a, b + 1):
+        arr[i] += 1
+print(max(arr))
